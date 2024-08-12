ab728f51fb779bfdb0ee9868eca9a2c0f74ae1cd
AminMehri <aminmehri45@gmail.com>
2024-08-12 16:43:57 +0330

    implement unit test for login view

--- Account/tests.py
+++ Account/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from Account.models import Account, User
 
+
+
 class RegisterViewTest(TestCase):
     
     def setUp(self):
@@ -54,5 +56,45 @@ class RegisterViewTest(TestCase):
         # Check message
         self.assertEqual(response.json().get('message'), 'username is already exist')
 
-        # Check user not created
-        self.assertFalse(Account.objects.filter(user__username='testuser').exists())
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('login')
+    
+    def test_login_user_success(self):
+        User.objects.create_user(username='testuser', password='testpass123', email='test.test@test.test')
+        response = self.client.post(self.url, {
+            'username': 'testuser',
+            'password': 'testpass123',
+        })
+
+        # Check status code
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_user_wrong_password(self):
+        User.objects.create_user(username='testuser', password='testpass123', email='test.test@test.test')
+        response = self.client.post(self.url, {
+            'username': 'testuser',
+            'password': 'testpass123456',
+        })
+
+        # Check status code
+        self.assertEqual(response.status_code, 401)
+
+        # Check message
+        self.assertEqual(response.json().get('detail'), "No active account found with the given credentials")
+
+    def test_register_user_wrong_username(self):
+        User.objects.create_user(username='testuser', password='testpass123', email='test.test@test.test')
+        response = self.client.post(self.url, {
+            'username': 'testuser123456',
+            'password': 'testpass123',
+        })
+
+        # Check status code
+        self.assertEqual(response.status_code, 401)
+
+        # Check message
+        self.assertEqual(response.json().get('detail'), "No active account found with the given credentials")
