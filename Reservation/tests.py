a5e452fcc4414a1d7a790ace3c48cc5f198e867d
AminMehri <aminmehri45@gmail.com>
2024-08-14 00:06:08 +0330

    improve unit tests

--- Reservation/tests.py
+++ Reservation/tests.py
@@ -1,33 +1,24 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from Reservation.models import Reservation, Doctor, Department, Patient
-from django.shortcuts import get_object_or_404
 from Account.models import Account, User
 
-
-def create_test_obj():
-    Department.objects.create(name='Dentistry', slug='test', description='test', id=1)
-    dep = get_object_or_404(Department, id=1)
-    Doctor.objects.create(full_name='test' ,department=dep ,slug='test' ,description='test' ,capacity=20 ,phone='test' ,office_address='test', id=1)
-    doc = get_object_or_404(Doctor, id=1)
-    User.objects.create_user(username='testuser', password='testpass123', email='test.test@test.test')
-    user = get_object_or_404(User, username='testuser')
-    Account.objects.create(user=user, id=1)
-    acc = get_object_or_404(Account, id=1)
-    Patient.objects.create(account=acc, full_name='test', gender='m', phone='test', address='test')
-    pat = get_object_or_404(Patient, id=1)
-
-    return pat, doc
 
 
 class GetTicketViewTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.url = reverse('get-ticket') 
-    
-    def test_get_ticket_success(self):
 
-        pat, doc = create_test_obj()
+        self.department = Department.objects.create(name='Dentistry', slug='test', description='test', id=1)
+        self.doctor = Doctor.objects.create(full_name='test' ,department=self.department ,slug='test' ,description='test' ,capacity=20 ,phone='test' ,office_address='test', id=1)
+        self.doctor2 = Doctor.objects.create(full_name='test2' ,department=self.department ,slug='test2' ,description='test2' ,capacity=20 ,phone='test2' ,office_address='test2', id=2)
+        self.user = User.objects.create_user(username='testuser', password='testpass123', email='test.test@test.test')
+        self.account = Account.objects.create(user=self.user, id=1)
+        self.patient = Patient.objects.create(account=self.account, full_name='test', gender='m', phone='test', address='test')
+        self.reservation = Reservation.objects.create(doctor=self.doctor2, patient=self.patient)
+
+    def test_get_ticket_success(self):
 
         response = self.client.post(self.url, {
             'patient_id': 1,
@@ -39,8 +30,6 @@ class GetTicketViewTest(TestCase):
     
     def test_get_ticket_invalid_p_id(self):
 
-        pat, doc = create_test_obj()
-
         response = self.client.post(self.url, {
             'patient_id': "a",
             'doctor_id': 1,
@@ -51,20 +40,15 @@ class GetTicketViewTest(TestCase):
     
     def test_get_ticket_exist_ticket(self):
 
-        pat, doc = create_test_obj()
-        Reservation.objects.create(doctor=doc, patient=pat)
-
         response = self.client.post(self.url, {
             'patient_id': 1,
-            'doctor_id': 1,
+            'doctor_id': 2,
         }, content_type="application/json")
         
         self.assertEqual(response.status_code, 208)
         self.assertEqual(response.json().get("message"), "reservation is alreay exist")
     
     def test_get_ticket_int_id(self):
-
-        pat, doc = create_test_obj()
 
         response = self.client.post(self.url, {
             'patient_id': "1",
@@ -73,3 +57,26 @@ class GetTicketViewTest(TestCase):
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json().get("message"), "patient id must be an integer")
+
+
+
+class ReservationModelTest(TestCase):
+
+    def setUp(self):
+        self.department = Department.objects.create(name='Dentistry', slug='test', description='test', id=1)
+        self.doctor = Doctor.objects.create(full_name='test' ,department=self.department ,slug='test' ,description='test' ,capacity=20 ,phone='test' ,office_address='test', id=1)
+        self.user = User.objects.create_user(username='testuser', password='testpass123', email='test.test@test.test')
+        self.account = Account.objects.create(user=self.user, id=1)
+        self.patient = Patient.objects.create(account=self.account, full_name='test', gender='m', phone='test', address='test')
+        self.reservation = Reservation.objects.create(doctor=self.doctor, patient=self.patient,)
+
+    def test_reservation_decreases_capacity(self):
+        
+        self.doctor.refresh_from_db()
+        self.assertEqual(self.doctor.capacity, 19)
+
+    def test_reservation_delete_decreases_capacity(self):
+
+        self.reservation.delete()
+        self.doctor.refresh_from_db()
+        self.assertEqual(self.doctor.capacity, 20)
